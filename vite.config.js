46ac3d788fc931f32b3a100c488444879aa7000c
wisnuadi <wisnuapp7@gmail.com>
2024-12-15 23:45:01 +0700

    Final Macro 4

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Festix-Event-Ticketing-Website/',
-  build: {
-    rollupOptions: {
-      external: ['fsevents'], // Tambahkan fsevents ke daftar eksternal
-    },
-  },
-});
+})
